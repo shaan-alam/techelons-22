1e68105e8a8b714aa8f9db6786ff7839958eab54
shaan-alam <shaanalam369@gmail.com>
2022-04-07 23:09:18 +0530

    fix: adjusted css of bg blur spans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,8 @@ function MyApp({ Component, pageProps }) {
     <div className="bg-black">
       {/* <Navbar /> */}
       <div className="h-screen w-full absolute inset-0 overflow-hidden">
-        {/* <span className="bg-[#36B1E2] opacity-80 absolute sm:top-0 sm:-left-28 top-11 -left-14 filter sm:blur-[200px] h-[200px] w-[200px] blur-[50px] sm:h-[400px] sm:w-[400px] rounded-full"></span> */}
-        {/* <span className="bg-[#FF02A1] opacity-80 absolute sm:top-0 sm:-right-28 top-11 -right-14 filter sm:blur-[200px] h-[200px] w-[200px] blur-[50px] sm:h-[400px] sm:w-[400px] rounded-full"></span> */}
+        <span className="bg-[#36B1E2] opacity-80 absolute sm:top-0 sm:-left-28 top-11 -left-24 filter sm:blur-[200px] h-[250px] w-[250px] blur-[40px] sm:h-[400px] sm:w-[400px] rounded-full"></span>
+        <span className="bg-[#FF02A1] opacity-80 absolute sm:top-0 sm:-right-28 top-11 -right-24 filter sm:blur-[200px] h-[250px] w-[250px] blur-[40px] sm:h-[400px] sm:w-[400px] rounded-full"></span>
       </div>
       <Component {...pageProps} />
     </div>
