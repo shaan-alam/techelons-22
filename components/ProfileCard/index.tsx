2a7cf8e3c3a50a0c388047ce4f01288254a8c3e5
shaan-alam <shaanalam369@gmail.com>
2022-04-08 22:18:21 +0530

    fix: lazy loading and styling changes

--- components/ProfileCard/index.tsx
+++ components/ProfileCard/index.tsx
@@ -1,3 +1,5 @@
+import LazyImage from "react-lazy-blur-image";
+
 interface Props {
   name: string;
   designation: string;
@@ -7,9 +9,17 @@ interface Props {
 const ProfileCard = ({ name, designation, imageURL }: Props) => {
   return (
     <div className="flex flex-col items-center">
-      <img src={imageURL} alt={name} className="w-3/4" />
+      <LazyImage
+        placeholder={imageURL}
+        uri={imageURL}
+        render={(src, style) => (
+          <img src={src} style={style} className="w-3/4" />
+        )}
+      />
       <h3 className="font-josefin text-white my-4 text-lg">{name}</h3>
-      <h3 className="font-josefin text-gray-400 text-sm -my-4">{designation}</h3>
+      <h3 className="font-josefin text-gray-400 text-sm -my-4">
+        {designation}
+      </h3>
     </div>
   );
 };
