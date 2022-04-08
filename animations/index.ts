cf22faf811d7be5564fee82245f92c4e1268b018
shaan-alam <shaanalam369@gmail.com>
2022-04-08 17:40:59 +0530

    feat: page transition

---
+++ animations/index.ts
@@ -0,0 +1,15 @@
+export const pageTransition = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeIn",
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
