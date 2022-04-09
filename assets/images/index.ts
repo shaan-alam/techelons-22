56b1a3563fe4f75b3ee9188f1108b7575244a663
shaan-alam <shaanalam369@gmail.com>
2022-04-09 22:27:36 +0530

    feat: separate folder for svg icons

---
+++ assets/images/index.ts
@@ -0,0 +1,17 @@
+import code from "./code.svg";
+import cpu from "./cpu.svg";
+import drive from "./drive.svg";
+import laptop from "./laptop.svg";
+import logo_black from "./logo_black.svg";
+import logo from "./logo.svg";
+import small_logo from "./small_logo.svg";
+
+export {
+  code,
+  cpu,
+  drive,
+  laptop,
+  logo_black,
+  logo,
+  small_logo,
+};
