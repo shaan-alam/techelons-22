fd92019e36037eaaa8f4478a27700eccfee2068d
shaan-alam <shaanalam369@gmail.com>
2022-04-09 17:07:06 +0530

    profileCard component default export

--- components/index.tsx
+++ components/index.tsx
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as Button } from "./Button";
 export { default as Logo } from "./Logo";
-export { default as ProfileCard } from './ProfileCard';
+export { default as ProfileCard } from "./ProfileCard";
+export { default as ParallaxImages } from "./ParallaxImages";
