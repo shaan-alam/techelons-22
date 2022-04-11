e9c329d2614f992ef4e0184aa944839f885a8b80
shaan-alam <shaanalam369@gmail.com>
2022-04-11 07:57:28 +0530

    code refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.scss";
 import { ParallaxProvider } from "react-scroll-parallax";
 import NextNProgress from "nextjs-progressbar";
-import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ParallaxProvider>
+
         <div className="h-screen w-full bg-[#000] fixed inset-0 z-[-1]">
           <span className="absolute top-[-7rem] lg:top-[-80%] lg:right-[25vw] h-[200px] w-full lg:h-[600px] lg:w-[600px] bg-gradient-to-r from-[#0719b9] to-[#EE5DB4] filter blur-[100px] opacity-80 rounded-full"></span>
         </div>
